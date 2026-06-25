a0f95ee27bf0ab0e74285f78fab5a2df8bb8937c
koolkrafter5 <creepersrgreen@gmail.com>
2026-06-25 17:01:10 -0400

    Drain power from electric hoe of growth instead of damaging it (#100)

--- src/main/java/thaumic/tinkerer/common/compat/EMTCompat.java
+++ src/main/java/thaumic/tinkerer/common/compat/EMTCompat.java
@@ -1,9 +1,12 @@
 package thaumic.tinkerer.common.compat;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.registry.GameRegistry;
+import ic2.api.item.ElectricItem;
 
 public class EMTCompat {
 
@@ -12,5 +15,13 @@ public class EMTCompat {
 
     public static void init() {
         electricHoe = GameRegistry.findItem("EMT", "ElectricHoeGrowth");
+    }
+
+    public static boolean validElectricHoe(ItemStack stack) {
+        return EMTLoaded && stack.getItem() == EMTCompat.electricHoe && ElectricItem.manager.canUse(stack, 250);
+    }
+
+    public static void useEnergy(ItemStack hoeOfGrowth, EntityPlayer player) {
+        ElectricItem.manager.use(hoeOfGrowth, 250, player);
     }
 }
