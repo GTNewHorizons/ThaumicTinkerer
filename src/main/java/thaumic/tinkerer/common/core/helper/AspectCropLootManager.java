86b7401e21cfd3bfda882cd3f7e8973a6ebedbd5
LazyFlesh <40587985+LazyFlesh@users.noreply.github.com>
2026-08-27 21:08:06 -0700

    add some seed drops

--- src/main/java/thaumic/tinkerer/common/core/helper/AspectCropLootManager.java
+++ src/main/java/thaumic/tinkerer/common/core/helper/AspectCropLootManager.java
@@ -152,10 +152,12 @@ public class AspectCropLootManager {
         addAspectLoot(Aspect.MIND, new ItemStack(Items.paper, 64), 15);
         addAspectLoot(Aspect.MIND, new ItemStack(Items.book, 32), 10);
         addAspectLoot(Aspect.MIND, new ItemStack(Blocks.bookshelf, 16), 5);
+        addAspectLoot(Aspect.MIND, new ItemStack(ConfigItems.itemResource, 4, 9), 5);
 
         addAspectLoot(Aspect.FLESH, new ItemStack(ConfigItems.itemResource, 16, 5), 1);
 
         addAspectLoot(Aspect.UNDEAD, new ItemStack(Items.rotten_flesh, 32));
+        addAspectLoot(Aspect.UNDEAD, new ItemStack(Items.bone, 24));
 
         addAspectLoot(
                 Aspect.CRAFT,
@@ -169,7 +171,6 @@ public class AspectCropLootManager {
         addAspectLoot(Aspect.COLD, new ItemStack(Items.snowball, 64));
         addAspectLoot(Aspect.COLD, new ItemStack(Blocks.snow, 32));
         addAspectLoot(Aspect.COLD, new ItemStack(Blocks.snow_layer, 8));
-        addAspectLoot(Aspect.COLD, "rodBlizz", 1);
 
         addAspectLoot(Aspect.PLANT, "grass");
         addAspectLoot(Aspect.PLANT, "sapling");
@@ -189,9 +190,10 @@ public class AspectCropLootManager {
         addAspectLoot(Aspect.TOOL, new ItemStack(ConfigItems.itemShovelThaumium));
         addAspectLoot(Aspect.TOOL, new ItemStack(ConfigItems.itemAxeThaumium));
 
-        addAspectLoot(Aspect.SLIME, "slime");
+        addAspectLoot(Aspect.SLIME, "slime", 4);
 
         addAspectLoot(Aspect.GREED, new ItemStack(Items.gold_ingot, 4));
+        addAspectLoot(Aspect.GREED, new ItemStack(Items.emerald, 2));
 
         addAspectLoot(Aspect.LIGHT, new ItemStack(Items.glowstone_dust, 16), 5);
         addAspectLoot(Aspect.LIGHT, new ItemStack(ConfigItems.itemResource, 4, 1));
@@ -200,8 +202,12 @@ public class AspectCropLootManager {
                 new ItemStack(ThaumicTinkerer.registry.getFirstItemFromClass(ItemBrightNitor.class)));
 
         addAspectLoot(Aspect.MECHANISM, new ItemStack(Blocks.piston, 8));
+        addAspectLoot(Aspect.MECHANISM, new ItemStack(Blocks.hopper, 4));
+        addAspectLoot(Aspect.MECHANISM, new ItemStack(Blocks.dropper, 8));
+        addAspectLoot(Aspect.MECHANISM, new ItemStack(Blocks.dispenser, 8));
 
         addAspectLoot(Aspect.CROP, new ItemStack(Items.wheat, 32));
+        addAspectLoot(Aspect.CROP, "seed");
 
         addAspectLoot(Aspect.METAL, new ItemStack(Items.iron_ingot, 4), 100);
 
@@ -241,8 +247,6 @@ public class AspectCropLootManager {
         }
 
         addAspectLoot(Aspect.SOUL, new ItemStack(Blocks.soul_sand, 16), 2);
-        addAspectLoot(Aspect.SOUL, new ItemStack(Blocks.netherrack, 32), 2);
-        addAspectLoot(Aspect.SOUL, new ItemStack(Blocks.nether_brick));
 
         addAspectLoot(Aspect.WEATHER, "cloud", 64);
 
@@ -251,17 +255,23 @@ public class AspectCropLootManager {
         addAspectLoot(Aspect.VOID, new ItemStack(Items.bucket));
         addAspectLoot(Aspect.VOID, "bucket");
         addAspectLoot(Aspect.VOID, "bowl");
+        addAspectLoot(Aspect.VOID, new ItemStack(ConfigItems.itemResource, 1, 17)); // void seed
 
         addAspectLoot(Aspect.POISON, new ItemStack(ConfigItems.itemResource, 16, 3));
+        addAspectLoot(Aspect.POISON, new ItemStack(Items.spider_eye, 8, 1));
 
         addAspectLoot(Aspect.LIFE, new ItemStack(Items.egg, 8));
 
         addAspectLoot(Aspect.TRAP, new ItemStack(Blocks.web, 4));
         addAspectLoot(Aspect.TRAP, "trapdoorWood");
+        new ItemStack(ConfigItems.itemResource, 16, 6); // amber
+
+        addAspectLoot(Aspect.TRAVEL, new ItemStack(ConfigBlocks.blockCosmeticSolid, 4, 2));
 
         addAspectLoot(Aspect.TAINT, new ItemStack(ConfigItems.itemResource, 4, 11));
 
-        addAspectLoot(Aspect.CRYSTAL, "gemDiamond");
+        addAspectLoot(Aspect.CRYSTAL, new ItemStack(Items.diamond, 1));
+        addAspectLoot(Aspect.CRYSTAL, new ItemStack(Items.quartz, 1));
         addAspectLoot(Aspect.CRYSTAL, new ItemStack(Blocks.glass), 100);
     }
 }
